e7ed567e5fd7774c0218dd99fe1e562fd4fdf919
Matthew Johnson <mattmjohnson33@gmail.com>
2017-03-06 22:43:01 -0800

    Wrote checkDuplicates method and 3rd test

--- source/identify-duplicates.js
+++ source/identify-duplicates.js
@@ -1,5 +1,5 @@
 class IdentifyDuplicates {
-    
+    checkDuplicates(){}
 }
 
 export default IdentifyDuplicates;
--- test/identify-duplicates.test.js
+++ test/identify-duplicates.test.js
@@ -13,4 +13,11 @@ describe("IdentifyDuplicates", () => {
 
         expect(identifyDuplicates.checkDuplicates()).to.be.defined;
     });
+
+    it("should return the number of letters which have duplicates", () => {
+        let identifyDuplicates = new IdentifyDuplicates();
+
+        expect(identifyDuplicates.checkDuplicates('abcde')).to.equal("0 # no characters repeats more than once");
+        expect(identifyDuplicates.checkDuplicates('aabbcde')).to.equal("2 # 'a' and 'b'");
+    });
 });
